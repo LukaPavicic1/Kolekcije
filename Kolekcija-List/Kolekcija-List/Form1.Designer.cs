8e36fb415ff7c239f238c2fe8dc050a4f988a40c
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-02-08 11:14:39 +0100

    Add files via upload

---
+++ Kolekcija-List/Kolekcija-List/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Kolekcija_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnos = new System.Windows.Forms.Button();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelRazred = new System.Windows.Forms.Label();
+            this.cmbRazred = new System.Windows.Forms.ComboBox();
+            this.btnPregled = new System.Windows.Forms.Button();
+            this.btnObrada = new System.Windows.Forms.Button();
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnUnos
+            // 
+            this.btnUnos.Location = new System.Drawing.Point(284, 23);
+            this.btnUnos.Name = "btnUnos";
+            this.btnUnos.Size = new System.Drawing.Size(168, 97);
+            this.btnUnos.TabIndex = 0;
+            this.btnUnos.Text = "UNOS";
+            this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.btnUnos_Click);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(77, 23);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(163, 20);
+            this.txtIme.TabIndex = 1;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(77, 63);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(163, 20);
+            this.txtPrezime.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ime:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prezime:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelRazred
+            // 
+            this.labelRazred.AutoSize = true;
+            this.labelRazred.Location = new System.Drawing.Point(23, 102);
+            this.labelRazred.Name = "labelRazred";
+            this.labelRazred.Size = new System.Drawing.Size(44, 13);
+            this.labelRazred.TabIndex = 5;
+            this.labelRazred.Text = "Razred:";
+            // 
+            // cmbRazred
+            // 
+            this.cmbRazred.FormattingEnabled = true;
+            this.cmbRazred.Items.AddRange(new object[] {
+            "1.",
+            "2.",
+            "3.",
+            "4."});
+            this.cmbRazred.Location = new System.Drawing.Point(77, 99);
+            this.cmbRazred.Name = "cmbRazred";
+            this.cmbRazred.Size = new System.Drawing.Size(163, 21);
+            this.cmbRazred.TabIndex = 7;
+            this.cmbRazred.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnPregled
+            // 
+            this.btnPregled.Location = new System.Drawing.Point(458, 23);
+            this.btnPregled.Name = "btnPregled";
+            this.btnPregled.Size = new System.Drawing.Size(153, 97);
+            this.btnPregled.TabIndex = 8;
+            this.btnPregled.Text = "PREGLED";
+            this.btnPregled.UseVisualStyleBackColor = true;
+            this.btnPregled.Click += new System.EventHandler(this.btnPregled_Click);
+            // 
+            // btnObrada
+            // 
+            this.btnObrada.Location = new System.Drawing.Point(617, 23);
+            this.btnObrada.Name = "btnObrada";
+            this.btnObrada.Size = new System.Drawing.Size(171, 97);
+            this.btnObrada.TabIndex = 9;
+            this.btnObrada.Text = "Obrada";
+            this.btnObrada.UseVisualStyleBackColor = true;
+            this.btnObrada.Click += new System.EventHandler(this.btnObrada_Click);
+            // 
+            // rtbIspis
+            // 
+            this.rtbIspis.Location = new System.Drawing.Point(20, 140);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(768, 298);
+            this.rtbIspis.TabIndex = 10;
+            this.rtbIspis.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbIspis);
+            this.Controls.Add(this.btnObrada);
+            this.Controls.Add(this.btnPregled);
+            this.Controls.Add(this.cmbRazred);
+            this.Controls.Add(this.labelRazred);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.btnUnos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnos;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelRazred;
+        private System.Windows.Forms.ComboBox cmbRazred;
+        private System.Windows.Forms.Button btnPregled;
+        private System.Windows.Forms.Button btnObrada;
+        private System.Windows.Forms.RichTextBox rtbIspis;
+    }
+}
+
---
+++ Kolekcija-List/Kolekcija-List/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolekcija_List
+{
+    public partial class Form1 : Form
+    {
+        List<Ucenik> ucenici = new List<Ucenik>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ucenik ucenik = new Ucenik(txtIme.Text, txtPrezime.Text, cmbRazred.Text);
+                ucenici.Add(ucenik);
+                txtIme.Clear();
+                txtPrezime.Clear();
+                MessageBox.Show("Uspješan unos!",
+                "Obavijest", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            }
+            catch (Exception greska)
+            {
+
+                MessageBox.Show("Pogrešan unos!\r\n" + greska.Message,
+                "Greška", MessageBoxButtons.OK,
+                MessageBoxIcon.Error); 
+            }
+        }
+
+        private void btnPregled_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Text = "Ime" + "\t\tPrezime" + "\t\tRazred\n\r";
+            foreach (Ucenik ucenik in ucenici)
+            {
+                rtbIspis.AppendText(ucenik.ToString()); 
+            }
+        }
+
+        private void btnObrada_Click(object sender, EventArgs e)
+        {
+            foreach (Ucenik ucenik in ucenici)
+            {
+                if (ucenik.Razred == "2.")
+                {
+                    ucenik.Razred = "3.";
+                }  
+            }
+            rtbIspis.AppendText("-----Obrada-----" + "\r\n");
+            foreach (Ucenik ucenik in ucenici)
+            {
+                rtbIspis.AppendText(ucenik.ToString() + "\r\n");
+            }
+
+        }
+    }
+}
